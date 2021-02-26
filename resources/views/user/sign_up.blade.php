f76b4adbb4174b3a9c6d42e2dbf3316c4757b513
Tatsuya Fujihara <pjpktwagtdjt@gmail.com>
2021-02-26 11:47:42 +0000

    Laravel課題2-edit1

--- resources/views/user/sign_up.blade.php
+++ resources/views/user/sign_up.blade.php
@@ -25,28 +25,28 @@
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
                             <div class="col-md-6">
-                              <input type="name" id="name" class="form-control" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                              <input type="text" id="name" class="form-control" name="name" value="{{ old('name') }}" autofocus>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
                             <div class="col-md-6">
-                              <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                              <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
                             <div class="col-md-6">
-                              <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}">
+                              <input type="text" class="form-control" id="password" name="password" value="{{ old('password') }}">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="confirm password" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
                             <div class="col-md-6">
-                              <input type="confirm password" class="form-control" id="confirm password" name="password_confirmation">
+                              <input type="text" class="form-control" id="confirm password" name="password_confirmation">
                             </div>
                         </div>
 
